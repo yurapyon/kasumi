58f3c3046d07a7808fa00a16647eff72e1647686
extrasharp <genericpb@gmail.com>
2020-08-28 19:04:15 -0500

    better graph

--- src/graph.zig
+++ src/graph.zig
@@ -4,77 +4,71 @@ const ArrayList = std.ArrayList;
 const NodeIndex = usize;
 const EdgeIndex = usize;
 
-const EdgeMax = std.math.maxInt(EdgeIndex);
+const Direction = enum(usize) {
+    const Self = @This();
 
-fn Node(comptime NodeWeight: type) type {
-    return struct {
-        weight: NodeWeight,
-        next_outgoing: EdgeIndex,
-        next_incoming: EdgeIndex,
-    };
-}
+    Outgoing,
+    Incoming,
 
-fn Edge(comptime EdgeWeight: type) type {
-    return struct {
-        weight: EdgeWeight,
-
-        start_node: NodeIndex,
-        end_node: NodeIndex,
-
-        next_outgoing: EdgeIndex,
-        next_incoming: EdgeIndex,
-    };
-}
-
-fn EdgeIterator(comptime NodeWeight: type, comptime EdgeWeight: type) type {
-    return struct {
-        const Self = @This();
-
-        graph: *Graph(NodeWeight, EdgeWeight),
-        curr_idx: EdgeIndex,
-        do_outgoing: bool,
-
-        fn init(graph: *Graph(NodeWeight, EdgeWeight), start_idx: EdgeIndex, do_outgoing: bool) Self {
-            return Self{
-                .graph = graph,
-                .curr_idx = start_idx,
-                .do_outgoing = do_outgoing,
-            };
-        }
-
-        pub fn next(self: *Self) ?*Edge(EdgeWeight) {
-            if (self.curr_idx == EdgeMax) {
-                return null;
-            } else {
-                const edge = &self.graph.edges.items[self.curr_idx];
-                const next_idx = if (self.do_outgoing) edge.next_outgoing else edge.next_incoming;
-                self.curr_idx = next_idx;
-                return edge;
-            }
-        }
-    };
-}
-
-const GraphError = error{
-    NodeIndexOutOfBounds,
-    EdgeIndexOutOfBounds,
+    fn opposite(self: Self) Self {
+        return @intToEnum(Self, (@enumToInt(self) + 1) % 2);
+    }
 };
 
-pub fn Graph(comptime NodeWeight: type, comptime EdgeWeight: type) type {
+/// data can be accessed by going like graph.nodes.items[some_idx]
+pub fn Graph(comptime N: type, comptime E: type) type {
     return struct {
         const Self = @This();
-        const NodeList = ArrayList(Node(NodeWeight));
-        const EdgeList = ArrayList(Edge(EdgeWeight));
 
-        allocator: *std.mem.Allocator,
-        nodes: NodeList,
-        edges: EdgeList,
+        const Node = struct {
+            weight: N,
+
+            next: ?NodeIndex,
+
+            edges: [2]?EdgeIndex,
+        };
+
+        const Edge = struct {
+            weight: E,
+
+            start_node: NodeIndex,
+            end_node: NodeIndex,
+
+            next: [2]?EdgeIndex,
+        };
+
+        const EdgeReference = struct {
+            idx: EdgeIndex,
+            edge: *Edge,
+        };
+
+        const EdgeIterator = struct {
+            graph: *const Self,
+            curr_idx: ?EdgeIndex,
+            direction: Direction,
+
+            pub fn next(self: *EdgeIterator) ?EdgeReference {
+                const idx = self.curr_idx orelse return null;
+                const edge = &self.graph.edges.items[idx];
+                self.curr_idx = edge.next[@enumToInt(self.direction)];
+                return EdgeReference{
+                    .edge = edge,
+                    .idx = idx,
+                };
+            }
+        };
+
+        nodes: ArrayList(Node),
+        edges: ArrayList(Edge),
+        next_node: ?NodeIndex,
+        next_edge: ?EdgeIndex,
 
         pub fn init(allocator: *std.mem.Allocator) Self {
             return Self{
-                .allocator = allocator,
-                .nodes = NodeList.init(allocator),
-                .edges = EdgeList.init(allocator),
+                .nodes = ArrayList(Node).init(allocator),
+                .edges = ArrayList(Edge).init(allocator),
+                .next_node = null,
+                .next_edge = null,
             };
         }
 
@@ -83,55 +77,162 @@ pub fn Graph(comptime NodeWeight: type, comptime EdgeWeight: type) type {
             self.edges.deinit();
         }
 
-        pub fn add_node(self: *Self, weight: NodeWeight) !NodeIndex {
-            const new_id = self.nodes.items.len;
-            try self.nodes.append(.{
-                .weight = weight,
-                .next_outgoing = std.math.maxInt(EdgeIndex),
-                .next_incoming = std.math.maxInt(EdgeIndex),
-            });
-            return new_id;
+        fn removeEdgeFromNode(
+            self: *Self,
+            node_idx: NodeIndex,
+            edge_idx: EdgeIndex,
+            direction: Direction,
+        ) void {
+            const edge = &self.edges.items[edge_idx];
+            var node = &self.nodes.items[node_idx];
+            const dir = @enumToInt(direction);
+
+            if (node.edges[dir]) |head_idx| {
+                if (head_idx == edge_idx) {
+                    node.edges[dir] = edge.next[dir];
+                } else {
+                    var find_idx: ?usize = head_idx;
+                    while (find_idx) |find_idx_| {
+                        var find_edge = self.edges.items[_find_idx_];
+
+                        if (find_edge.next[dir] == edge_idx) {
+                            find_edge.next = edge.next;
+                            break;
+                        }
+
+                        find_idx = find_edge.next[dir];
+                    }
+                }
+            }
         }
 
-        pub fn add_edge(
+        pub fn addNode(self: *Self, weight: N) !NodeIndex {
+            const idx = if (self.next_node) |idx| blk: {
+                var node = &self.nodes.items[idx];
+                self.next_node = node.next;
+                break :blk idx;
+            } else blk: {
+                const idx = self.nodes.items.len;
+                try self.nodes.append(undefined);
+                break :blk idx;
+            };
+
+            self.nodes.items[idx] = Node{
+                .weight = weight,
+                .next = null,
+                .edges = .{ null, null },
+            };
+
+            return idx;
+        }
+
+        pub fn removeNode(self: *Self, idx: NodeIndex) void {
+            {
+                var iter = self.edgesDirected(idx, .Outgoing);
+                while (iter.next()) |ref| {
+                    self.removeEdgeFromNode(ref.edge.end_node, ref.idx, .Incoming);
+                }
+            }
+
+            {
+                var iter = self.edgesDirected(idx, .Incoming);
+                while (iter.next()) |ref| {
+                    self.removeEdgeFromNode(ref.edge.start_node, ref.idx, .Outgoing);
+                }
+            }
+
+            self.nodes.items[idx].next = self.next_node;
+            self.next_node = idx;
+        }
+
+        pub fn addEdge(
             self: *Self,
             start_idx: NodeIndex,
             end_idx: NodeIndex,
-            weight: EdgeWeight,
+            weight: E,
         ) !EdgeIndex {
-            if (start_idx >= self.nodes.items.len or
-                end_idx >= self.nodes.items.len)
-            {
-                return error.NodeIndexOutOfBounds;
-            }
+            var node_start = &self.nodes.items[start_idx];
+            var node_end = &self.nodes.items[end_idx];
 
-            const new_id = self.edges.items.len;
-            var node_a = &self.nodes.items[start_idx];
-            var node_b = &self.nodes.items[end_idx];
+            const idx = if (self.next_edge) |idx| blk: {
+                var edge = &self.edges.items[idx];
+                self.next_edge = edge.next[0];
+                break :blk idx;
+            } else blk: {
+                const idx = self.edges.items.len;
+                try self.edges.append(undefined);
+                break :blk idx;
+            };
 
-            var edge = .{
+            self.edges.items[idx] = Edge{
                 .weight = weight,
                 .start_node = start_idx,
                 .end_node = end_idx,
-                .next_outgoing = node_a.next_outgoing,
-                .next_incoming = node_b.next_incoming,
+                .next = .{
+                    node_start.edges[@enumToInt(Direction.Outgoing)],
+                    node_end.edges[@enumToInt(Direction.Incoming)],
+                },
             };
 
-            node_a.next_outgoing = new_id;
-            node_b.next_incoming = new_id;
+            node_start.edges[@enumToInt(Direction.Outgoing)] = idx;
+            node_end.edges[@enumToInt(Direction.Incoming)] = idx;
 
-            try self.edges.append(edge);
-            return new_id;
+            return idx;
         }
 
-        pub fn edges_directed(
-            self: *Self,
-            node_idx: NodeIndex,
-            do_outgoing: bool,
-        ) EdgeIterator(NodeWeight, EdgeWeight) {
-            const node = self.nodes.items[node_idx];
-            const start_idx = if (do_outgoing) node.next_outgoing else node.next_incoming;
-            return EdgeIterator(NodeWeight, EdgeWeight).init(self, start_idx, do_outgoing);
+        pub fn removeEdge(self: *Self, idx: EdgeIndex) void {
+            var edge = &self.edges.items[idx];
+
+            self.removeEdgeFromNode(edge.start_node, idx, .Outgoing);
+            self.removeEdgeFromNode(edge.start_node, idx, .Incoming);
+            self.removeEdgeFromNode(edge.end_node, idx, .Outgoing);
+            self.removeEdgeFromNode(edge.end_node, idx, .Incoming);
+
+            edge.next[0] = self.next_edge;
+            self.next_edge = idx;
+        }
+
+        pub fn edgesDirected(
+            self: Self,
+            idx: NodeIndex,
+            direction: Direction,
+        ) EdgeIterator {
+            const node = self.nodes.items[idx];
+            const start_idx = node.edges[@enumToInt(direction)];
+            return EdgeIterator{
+                .graph = &self,
+                .curr_idx = start_idx,
+                .direction = direction,
+            };
         }
     };
+}
+
+const testing = std.testing;
+const expect = testing.expect;
+
+test "graph" {
+    const alloc = testing.allocator;
+
+    var g = Graph(u8, u8).init(alloc);
+    defer g.deinit();
+
+    const a = try g.addNode('a');
+    const b = try g.addNode('b');
+    const c = try g.addNode('c');
+    const d = try g.addNode('d');
+    const e = try g.addNode('e');
+
+    const e1 = try g.addEdge(a, b, 1);
+    const e2 = try g.addEdge(b, c, 2);
+    const e3 = try g.addEdge(a, d, 3);
+    const e4 = try g.addEdge(c, d, 4);
+    const e5 = try g.addEdge(d, e, 5);
+
+    g.removeEdge(e4);
+
+    var edges = g.edgesDirected(a, .Outgoing);
+    while (edges.next()) |ref| {
+        std.log.warn("\n{}\n", .{ref.edge});
+    }
 }
